89bf699ba9d7ff0bd70f3121d941f4fff1fa4f78
Treeed <freddyalleswegmail.com>
2020-07-27 18:22:36 +0200

    added temperature reading and turn off

--- main/analog_reader.c
+++ main/analog_reader.c
@@ -8,6 +8,7 @@
 #include <driver/adc.h>
 #include <esp_log.h>
 #include <esp_adc_cal.h>
+#include <math.h>
 #include "analog_reader.h"
 #include "spi_controller.h"
 #include "error_handler.h"
@@ -18,12 +19,19 @@ void set_mux(uint8_t value);
 uint32_t get_analog();
 uint32_t calc_battery_voltage(uint32_t adc_val);
 void check_battery_voltage(uint32_t voltage);
+double calc_PTC_temp(uint32_t adc_volt_mv);
+double calc_NTC_temp(uint32_t adc_volt_mv);
 
 #define MUX_PIN_BATTERY_VOLT 5
+#define MUX_PIN_ESC_TEMP 6
+#define MUX_PIN_BUCK_TEMP 4
+#define MUX_PIN_MOTOR_TEMP 2
 
 _Noreturn void read_analog_mux(void *pvParameters){
-    esp_adc_cal_characteristics_t adc_characteristics;
-    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &adc_characteristics);
+    esp_adc_cal_characteristics_t adc_characteristics_11db;
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 0, &adc_characteristics_11db);
+    esp_adc_cal_characteristics_t adc_characteristics_6db;
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_6, ADC_WIDTH_BIT_12, 0, &adc_characteristics_6db);
 
 
     while (1) {
@@ -35,8 +43,40 @@ _Noreturn void read_analog_mux(void *pvParameters){
         uint32_t bat_voltage = calc_battery_voltage(avg);
         check_battery_voltage(bat_voltage);
 
+        set_mux(MUX_PIN_ESC_TEMP);
+        avg = get_analog();
+        uint32_t voltage_mv = esp_adc_cal_raw_to_voltage(avg, &adc_characteristics_11db);
+        double esc_temp = calc_PTC_temp(voltage_mv);
 
+        set_mux(MUX_PIN_BUCK_TEMP);
+        avg = get_analog();
+        voltage_mv = esp_adc_cal_raw_to_voltage(avg, &adc_characteristics_11db);
+        double buck_temp = calc_PTC_temp(voltage_mv);
+
+        adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_6);
+        set_mux(MUX_PIN_MOTOR_TEMP);
+        avg = get_analog();
+        voltage_mv = esp_adc_cal_raw_to_voltage(avg, &adc_characteristics_6db);
+//        printf("volt %d\n", voltage_mv);
+        double motor_temp = calc_NTC_temp(voltage_mv);
+
+        if(esc_temp > 80 || buck_temp > 80 || motor_temp > 80){
+            ErrorType active_errors;
+            xQueuePeek(active_errors_queue, &active_errors, portMAX_DELAY);
+            if(!active_errors.battery_empty) {
+                ErrorType error_type = {0};
+                error_type.battery_empty = 1;
+                ErrorStruct error = {
+                        .error_type = error_type,
+                        .error_msg = "temp low"
+                };
+                xQueueSend(error_queue, &error, 0);
+            }
+        }
         printf("%u bat volt \n", bat_voltage);
+//        printf("%fC ",esc_temp);
+//        printf("%fC ",buck_temp);
+//        printf("%fC\n",motor_temp);
 
         xQueueOverwrite(analog_value_queue, &bat_voltage);
 
@@ -67,8 +107,19 @@ uint32_t calc_battery_voltage(uint32_t adc_val){
     return adc_val*4.584+28824; //accurate to within 20mV up to 42 V directly after cal
 }
 
+double calc_PTC_temp(uint32_t adc_volt_mv){
+    //uses 407.7669Ohms resistor to ground and kty81 210 upper https://www.nxp.com/docs/en/data-sheet/KTY81_SER.pdf
+    // 5/(x/407.8+1)=y x is resistance, y is voltage out
+    // y = 18,308x + 1549,7  y is resistance, x is temp
 
+    return 111372.0 / adc_volt_mv - 106.92;
+}
 
+double calc_NTC_temp(uint32_t adc_volt_mv){
+    // -90/(z - 5) - 18=y z is voltage y is resistance
+    // -26,65ln(x) + 269,59 = y x is resistance y is temp
+    return -26.65*log( -18000.0 - 90000000.0/(-5000.0 + adc_volt_mv)) + 269.59;
+}
 
 void check_battery_voltage(uint32_t voltage_mv){
     static int battery_low_cnt = 0;
